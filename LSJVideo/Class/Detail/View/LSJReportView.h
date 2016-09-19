8655a5bc6f98ec22b1ad203dbcaf361f31a6e043
Liang <q93867@yeah.net>
2016-09-19 19:05:50 +0800

    add report view in detailview

---
+++ LSJVideo/Class/Detail/View/LSJReportView.h
@@ -0,0 +1,13 @@
+//
+//  LSJReportView.h
+//  LSJVideo
+//
+//  Created by Liang on 16/9/19.
+//  Copyright © 2016年 iqu8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSJReportView : UIView
+
+@end
---
+++ LSJVideo/Class/Detail/View/LSJReportView.m
@@ -0,0 +1,21 @@
+//
+//  LSJReportView.m
+//  LSJVideo
+//
+//  Created by Liang on 16/9/19.
+//  Copyright © 2016年 iqu8. All rights reserved.
+//
+
+#import "LSJReportView.h"
+
+@implementation LSJReportView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
